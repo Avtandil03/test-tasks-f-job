483379c3018ad51b9d76c83e6bf4209f486b17dd
Avtandil <avtandilqazxswedc@gmail.com>
2023-03-23 18:10:12 +0300

    add default stuff

---
+++ future/book-searcher/src/App.jsx
@@ -0,0 +1,14 @@
+import BookList from './components/BookList/BookList'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <BookList />
+    </div>
+  )
+}
+
+export default App
